a6698b14145fa354c3a43b179abbc6f9834b8824
Diana Lepesbayeva <ldikosh01@gmail.com>
2023-10-04 01:19:28 +0300

    Storing Values with the Assignment Operator

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ undefined,null,boolean,string,symbol,bigint,number,and object
 */
 
 var myName;
+
+// Setup
+var a;
+var b = 7;
+console.log(a)
+a = 7;
+
+b = a;
+
+console.log(a)
+// Only change code below this line
+ 
